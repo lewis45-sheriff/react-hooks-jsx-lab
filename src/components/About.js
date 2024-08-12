0ab038677a2588abe34144091be58905927d76cf
lewis <lewiskipkemoi53@gmail.com>
2024-08-12 18:00:48 +0300

    all

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="about">
-    <h2>About Me</h2>
-    <p> My Name is Lewis KIpkemoi and i like coding</p>
-    <img src={image} alt= "i made this"></img>
-
-  </div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>My Name is Lewis Kipkemoi and I like coding</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>
-    <a href="#"> Home</a>
-    <a href="#"> About</a>
-  </nav>;
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  );
 }
 
 export default NavBar;
